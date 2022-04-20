caccb32957a3c342f8e2e6add84b2449f9ee428f
Jun Chen <junc76@gmail.com>
2022-04-20 22:40:33 +0800

    打印匹配的行

--- bmtool.py
+++ bmtool.py
@@ -10,7 +10,7 @@ class StringRep:
     def get_new_word(self, word) -> str:
         return  f"pfn{word[0].upper()}{word[1:]}"
 
-    def replace(self, match_obj):
+    def replace(self, match_obj) -> str:
         if match_obj.group(0) is not None:
             word = match_obj.group(0)[1:-1] # Remove the " in the first and end
             self.words.add(word)
@@ -18,22 +18,27 @@ class StringRep:
         else:
             return match_obj.group(0)
 
-    def parse(self, line) -> str:
+    def print_line(self, line, num) -> None:
+        print(f"{num}:{line[:-1]}")
+
+    def parse(self, line: str, num: int = 0) -> str:
         if re.search(r'#include.+\"(.+)\.h\"', line) or \
             re.search(r'\s.*DBG_WARN', line):
             return line
         else:
             # https://towardsdatascience.com/a-hidden-feature-of-python-regex-you-may-not-know-f00c286f4847
             new_line = self.matchObj.sub(self.replace, line)
+            if new_line != line:
+                self.print_line(line, num)
             return new_line
 
     def parse_file(self, file):
         filename = Path(file).name
-        newfile = Path(file).with_name(f"new{filename}")
+        newfile = Path(file).with_name(f"{filename}.bak")
         with open(newfile, "w") as nf:  
             with open(file, 'r') as f:
-                for line in f:
-                    new_line = self.parse(line, nf)
+                for num, line in enumerate(f, 1): # start count from 1
+                    new_line = self.parse(line, num)
                     nf.write(new_line)
 
 def main():
