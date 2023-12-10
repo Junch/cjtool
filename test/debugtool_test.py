5e37be9c20872e4ae17a2902cb416f3e01f71d36
chenj-bd <chenj-bd@glodon.com>
2023-12-10 09:35:41 +0800

    通过debugtool的单元测试

--- test/debugtool_test.py
+++ test/debugtool_test.py
@@ -1,7 +1,6 @@
 import unittest
 import subprocess
 from cjtool.debugtool import execute_command
-from cjtool.debugtool import match_the_deadly_pattern
 
 
 class execute_ommand_test(unittest.TestCase):
@@ -19,34 +18,14 @@ class execute_ommand_test(unittest.TestCase):
     def test_uf_command(self):
         self.assertTrue(
             execute_command(
-                'notepad.exe',
-                'uf notepad!RestartHandler::ShouldSupportRestartManager'))
+                'notepad.exe', 'uf notepad!TraceFileSaveStart'))
 
     def test_aaaaa_command(self):
         self.assertTrue(execute_command('notepad.exe', 'aaaaa'))
 
     def test_x_command(self):
         self.assertTrue(
-            execute_command('notepad.exe', 'x notepad!RestartHandler::*'))
+            execute_command('notepad.exe', 'x notepad!*Start'))
 
     def test_aaaaaaaaaaaaaaaaaaaa_command(self):
         self.assertTrue(execute_command('notepad.exe', 'aaaaaaaaaaaaaaaaaaaa'))
-
-
-class match_the_deadly_pattern_test(unittest.TestCase):
-
-    def test_simple(self):
-        self.assertTrue(match_the_deadly_pattern('00000000`0000bcde', 'abcde'))
-        self.assertTrue(
-            match_the_deadly_pattern('0000000a`aaaaaaaa', 'aaaaaaaaaa'))
-        self.assertTrue(match_the_deadly_pattern('00000000`0000aaaa', 'baaaa'))
-        self.assertTrue(
-            match_the_deadly_pattern('bbbbcccc`ddddeeee',
-                                     'aaaabbbbccccddddeeee'))
-
-        # 必须可以转成整数才match
-        self.assertFalse(match_the_deadly_pattern('00000000`0000aaaa',
-                                                  'qaaaa'))
-        self.assertFalse(match_the_deadly_pattern('00000000`0000bcde', 'bcde'))
-        self.assertFalse(match_the_deadly_pattern('00000000`a0000aaa',
-                                                  'aaaaa'))
